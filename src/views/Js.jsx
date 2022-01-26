7f232f8dbdbdedb1cfc7513072b6204be22012d1
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-01-26 16:21:28 +0100

    upload file

--- src/views/Js.jsx
+++ src/views/Js.jsx
@@ -4,6 +4,14 @@ const Js = () => {
   return (
     <div>
       <h1>JS stránka</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam nulla,
+        magni ut libero quod modi! Tempore sit odit neque voluptas. Tenetur,
+        nobis impedit quae doloribus maxime fuga porro! Laudantium ut nihil
+        omnis? Consequuntur, quia. Minus consequuntur voluptatem autem hic
+        praesentium reiciendis sequi voluptatibus velit quisquam quod, sit
+        dolore tempora unde!
+      </p>
     </div>
   );
 };
