4fdf39f3955e91a356df2e9cebe616ff28b760ef
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-01-29 17:15:29 +0100

    add .page-wraper for div

--- src/views/Css.jsx
+++ src/views/Css.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import "./views.scss";
 
 const Css = () => {
   return (
-    <div>
+    <div className="page-wrapper">
       <h1>CSS stránka ........</h1>
-      <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur itaque dicta doloremque deserunt, nisi voluptatibus architecto est quasi aliquam quae necessitatibus dolore explicabo non assumenda cupiditate aliquid aperiam autem harum.</h3>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
+        itaque dicta doloremque deserunt, nisi voluptatibus architecto est quasi
+        aliquam quae necessitatibus dolore explicabo non assumenda cupiditate
+        aliquid aperiam autem harum.
+      </h3>
     </div>
   );
 };
