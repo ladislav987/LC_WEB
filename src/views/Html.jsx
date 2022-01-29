2af6e05312a031d2ce697bb0f2f4019a7931b02b
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-01-29 09:31:15 +0100

    edit html page

--- src/views/Html.jsx
+++ src/views/Html.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+
+import "./views.scss";
+
+import Details from "./htmlSubpages/Details";
+import Summary from "./htmlSubpages/Summary";
+
 
 const Html = () => {
   return (
-    <div>
+    <div className="page-wrapper">
       <h1>HTML stránka</h1>
-      <h2>Skúška</h2>
+      <aside>
+        <ul>
+          <li>
+            <NavLink to="details">&#60;details&gt;</NavLink>
+          </li>
+          <li>
+            <NavLink to="summary">&#60;summary&gt;</NavLink>
+          </li>
+        </ul>
+      </aside>
+
+      <main>
+        <Routes>
+          <Route path="details" element={<Details />} />
+          <Route path="summary" element={<Summary />} />
+        </Routes>
+      </main>
     </div>
   );
 };
