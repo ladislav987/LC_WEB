dd9299f25a246d385b799d9e9535ebf233b10799
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-01-29 17:17:16 +0100

    add code exaple and description

--- src/views/htmlSubpages/Summary.jsx
+++ src/views/htmlSubpages/Summary.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Details = () => {
   return (
     <div>
-      <p>addad </p>
+      <h2>&#60;summary&gt;</h2>
+      <p>
+        &#60;summary&gt; je HTML elemente špecifický ako legenda pre{" "}
+        <NavLink to="/html/details">&#60;details&gt;</NavLink>{" "}
+      </p>
+
+      <div className="code-example">
+        <pre>
+          <code>{`<details>
+    <summary>Details</summary>
+    Something small enough to escape casual notice.
+</details>`}</code>
+        </pre>
+      </div>
     </div>
   );
 };
