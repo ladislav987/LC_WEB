1e6ff0a549067648bd9b366ae07e9c3c4948cb9f
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-01-30 08:24:15 +0100

    update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
-import App from "./App"
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
