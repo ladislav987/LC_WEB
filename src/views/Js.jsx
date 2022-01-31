526cac6128af8355d036323a485e89dde15e4807
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-01-31 12:49:49 +0100

    add subpage to Js.jsx

--- src/views/Js.jsx
+++ src/views/Js.jsx
@@ -6,6 +6,8 @@ import "./views.scss";
 import Promises from "./jsSubpages/Promises";
 import Async from "./jsSubpages/Async";
 import Fetch from "./jsSubpages/Fetch";
+import LogickeOperatory from "./jsSubpages/LogickeOperatory";
+import TernarnyOperator from "./jsSubpages/TernarnyOperator";
 
 const Js = () => {
   return (
@@ -17,9 +19,25 @@ const Js = () => {
           <li>
             <NavLink to="promises">Promises</NavLink>
           </li>
+
           <li>
             <NavLink to="async">Async</NavLink>
           </li>
+
+          <li>
+            <details>
+              <summary>Operátory</summary>
+              <ul className="toggle">
+                <li>
+                  <NavLink to="logickeOperatory">Logické Operátory</NavLink>
+                </li>
+                <li>
+                  <NavLink to="ternarnyOperator">Ternárny Operátor</NavLink>
+                </li>
+              </ul>
+            </details>
+          </li>
+
           <li>
             <details>
               <summary>Funkcie</summary>
@@ -37,6 +55,8 @@ const Js = () => {
         <Routes>
           <Route path="promises" element={<Promises />} />
           <Route path="async" element={<Async />} />
+          <Route path="logickeOperatory" element={<LogickeOperatory />} />
+          <Route path="ternarnyOperator" element={<TernarnyOperator />} />
           <Route path="fetch" element={<Fetch />} />
         </Routes>
       </main>
