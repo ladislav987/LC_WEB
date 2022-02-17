3a234f8fe1e87eff97e271cae523e8fbd25f16ac
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-02-17 17:00:00 +0100

    created animation

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-import "./Home.scss"
+import { useTransition, animated, useSpring } from "@react-spring/web";
+
+import "./Home.scss";
 
 const Home = () => {
+  const [items, setItems] = useState([]);
+
+  //? useTrantiton má dve parametre, prvá je naš useState a druhá je nastavovací objekt
+  const transition = useTransition(items, {
+    from: { x: -100, y: 500, opacity: 0 }, // predtým než sa namontuje
+    enter: (item) => async (next) => {
+      await next({ y: item.y, opacity: 1, delay: item.delay });
+      await next({ x: 0 });
+    }, // zobrazuje konečný stav
+    leave: { x: 100, y: 500, opacity: 0 }, // keď sa dokončí
+  });
+
   return (
     <div className="appSkuska">
-      <button className="buttonSkuska">Mount</button>
+      <button
+        className="buttonSkuska"
+        onClick={() => {
+          setItems((v) =>
+            v.length
+              ? []
+              : [
+                  { y: 0, delay: 200 },
+                  { y: 10, delay: 400 },
+                  { y: 20, delay: 600 },
+                ]
+          );
+        }}
+      >
+        {items.length ? "un-mount" : "mount"}
+      </button>
       <div className="containerSkuska">
-        <div className="item"></div>
+        {transition((style, item) =>
+          item ? <animated.div className="item" style={style} /> : ""
+        )}
       </div>
     </div>
   );
