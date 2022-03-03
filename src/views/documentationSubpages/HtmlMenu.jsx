b1f3d7c9f614afb474bfc362b767e8dd4fe725d4
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-03-03 08:23:46 +0100

    code clening / refactoring

--- src/views/documentationSubpages/HtmlMenu.jsx
+++ src/views/documentationSubpages/HtmlMenu.jsx
@@ -1,20 +1,24 @@
-import {NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const HtmlMenu = (props) => {
   const handleClick = (event) => {
     props.onClickToMenu();
-  }
+  };
   return (
-    <div>
+    <>
       <ul>
         <li>
-          <NavLink onClick={handleClick} to="details">&#60;details&gt;</NavLink>
+          <NavLink onClick={handleClick} to="details">
+            &#60;details&gt;
+          </NavLink>
         </li>
         <li>
-          <NavLink onClick={handleClick} to="summary">&#60;summary&gt;</NavLink>
+          <NavLink onClick={handleClick} to="summary">
+            &#60;summary&gt;
+          </NavLink>
         </li>
       </ul>
-    </div>
+    </>
   );
 };
 
