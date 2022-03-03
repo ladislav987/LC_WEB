20401d5d4b5d3b097667315ce9eec3a48f910824
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-03-03 11:34:46 +0100

    code refactoring

--- src/views/documentationSubpages/Js.jsx
+++ src/views/documentationSubpages/Js.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 
 import Svg from "../../component/Svg";
 import HomeButton from "../../component/HomeButton";
