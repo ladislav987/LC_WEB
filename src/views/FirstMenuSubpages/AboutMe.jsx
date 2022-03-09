9b864007d488bc5fed03f21cd21425af10df3413
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-03-09 22:14:21 +0100

    add code

--- src/views/FirstMenuSubpages/AboutMe.jsx
+++ src/views/FirstMenuSubpages/AboutMe.jsx
@@ -14,7 +14,43 @@ const AboutMe = () => {
           <ThemeButton />
         </nav>
       </header>
-      About Me !
+      <main className="skuska">
+        <img src={require("../../img/Foto.png")} alt="portrait" />
+        <div className="border">
+          <p>
+            Moje meno je <strong>Ladislav</strong>, študujem{" "}
+            <strong>informatiku</strong> na{" "}
+            <strong>Technickej Univerzite v Košiciach</strong>. Hľadám
+            príležitosť pracovať na projekte, kde si môžem zdokonaľovať svoje
+            poznatky a učiť sa novým veciam v IT.
+          </p>
+        </div>
+
+        <div className="border">
+          <h3>Vzdelanie a kurzy</h3>
+          <p>
+            2020 - súčasnosť Technická univerzita v Košiciach Fakulta
+            elektrotechniky a informatiky Odbor: informatika
+          </p>
+          <p>
+            2012 - 2016 Stredná odborná škola dopravná, Prešov Odbor: mechanik
+            elektrotechnik Ukončené: maturitná skúška
+          </p>
+          <p>2021 Kurz: Webrebel 1: HTML, CSS & JavaScript</p>
+        </div>
+
+        <div className="border">
+          <h3>Pracovné skúsenosti</h3>
+          <p>
+            1/2020 - Staffline universe, Bristol 9/2020 Pracovná pozícia:
+            pracovník v Tesco Distribution Centre
+          </p>
+        </div>
+
+        <div className="border">
+          <h3>Znalosti</h3>
+        </div>
+      </main>
     </>
   );
 };
