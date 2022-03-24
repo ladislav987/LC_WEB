fa15307b75fcf2b078693d5ee06811722436ccd7
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-03-24 20:51:27 +0100

    added files

--- src/views/FirstMenuSubpages/AboutMe.jsx
+++ src/views/FirstMenuSubpages/AboutMe.jsx
@@ -33,9 +33,9 @@ const AboutMe = () => {
       </header>
 
       <main className="container_aboutMe">
-        <section>
+        <section className="introduction">
           <img src={require("../../img/Foto.png")} alt="portrait" />
-          <p className="introduction">
+          <p>
             Moje meno je <strong>Ladislav</strong>, študujem{" "}
             <strong>informatiku</strong> na{" "}
             <strong>Technickej Univerzite v Košiciach</strong>. Hľadám
@@ -45,152 +45,155 @@ const AboutMe = () => {
           </p>
         </section>
 
-        <div className="border"></div>
+        <div className="border hidde"></div>
 
         <h3>Vzdelanie a kurzy</h3>
         <section className="vzdelanie">
-          <div className="grid_35_65">
-            <p className="grid1_1 bold">2020 - súčasnosť</p>
-            <p className="grid2_1">Technická univerzita v Košiciach </p>
-            <p className="grid2_3">Fakulta elektrotechniky a informatiky</p>
-            <p className="grid2_4">Odbor: informatika</p>
-          </div>
-          <div className="grid_35_65">
-            <p className="grid1_1 bold">2012 - 2016</p>
-            <p className="grid2_1"> Stredná odborná škola dopravná, Prešov</p>
-            <p className="grid2_3">Odbor: mechanik elektrotechnik</p>
-            <p className="grid2_4">Ukončené: maturitná skúška</p>
-          </div>
-          <div className="grid_35_65">
-            <p className="grid1_1 bold">2021</p>
-            <p className="grid2_1">Kurz: Webrebel 1: HTML, CSS & JavaScript</p>
-          </div>
+          <p className="vzdelanie_s1 bold">2020 - súčasnosť</p>
+          <p className="vzdelanie_s2">Technická univerzita v Košiciach </p>
+          <p className="vzdelanie_s3">Fakulta elektrotechniky a informatiky</p>
+          <p className="vzdelanie_s4">Odbor: informatika</p>
+          <p className="vzdelanie_s5 bold">2012 - 2016</p>
+          <p className="vzdelanie_s6">Stredná odborná škola dopravná, Prešov</p>
+          <p className="vzdelanie_s7">Odbor: mechanik elektrotechnik</p>
+          <p className="vzdelanie_s8">Ukončené: maturitná skúška</p>
+          <p className="vzdelanie_s9 bold">2021</p>
+          <p className="vzdelanie_s10">
+            Kurz: Webrebel 1: HTML, CSS & JavaScript
+          </p>
         </section>
 
-        <div className="border"></div>
+        <div className="border hidde"></div>
 
         <h3>Pracovné skúsenosti</h3>
         <section className="skusenosti">
-          <div className="grid_35_65">
-            <p className="grid1_1 bold">1/2020 - 9/2020</p>
-            <p className="grid2_1">Staffline universe, Bristol</p>
-            <p className="grid2_3">
-              Pracovná pozícia: pracovník v Tesco Distribution Centre
-            </p>
-          </div>
+          <p className="skusenosti_s1 bold">1/2020 - 9/2020</p>
+          <p className="skusenosti_s2">Staffline universe, Bristol</p>
+          <p className="skusenosti_s3">
+            Pracovná pozícia: pracovník v Tesco Distribution Centre
+          </p>
         </section>
 
-        <div className="border"></div>
+        <div className="border hidde"></div>
 
-        <h3>Znalosti</h3>
-        <section className="znalosti">
-          <div className="grid_15_45_40">
-            <span className="grid1_1">
-              <FaLanguage size="1.8em" />
-            </span>
-            <p className="grid2_1">Anglický jazyk</p>
-            <p className="grid3_1"> - B1</p>
-          </div>
-          <div className="grid_15_45_40">
-            <span className="grid1_1">
-              <DiHtml5 size="1.8em" />
-            </span>
-            <p className="grid2_1">HTML</p>
-            <p className="grid3_1"> - pokročilý</p>
-          </div>
-          <div className="grid_15_45_40">
-            <span className="grid1_1">
-              <DiCss3 size="1.8em" />
-            </span>
-            <p className="grid2_1">CSS</p>
-            <p className="grid3_1"> - pokročilý</p>
-          </div>
-          <div className="grid_15_45_40">
-            <span className="grid1_1">
-              <DiSass size="1.8em" />
-            </span>
-            <p className="grid2_1">SASS</p>
-            <p className="grid3_1"> - pokročilý</p>
-          </div>
-          <div className="grid_15_45_40">
-            <span className="grid1_1">
-              <DiJavascript1 size="1.8em" />
-            </span>
-            <p className="grid2_1">JavaScript</p>
-            <p className="grid3_1"> - pokročilý</p>
-          </div>
-        </section>
-
-        <div className="border"></div>
-
-        <div className="skuska">
-          <h3 className="skuska_grid1_1">Osobné údaje</h3>
-          <section className="udaje">
-            <div className="grid_15_85">
-              <span className="grid1_1 skuska_grid1_2">
-                <BsFillPersonFill size="1.8em" />
+        <div className="znalosti_udaje_zaujmy">
+          <section className="znalosti">
+            <h3 className="znalosti_s1">Znalosti</h3>
+            <div className="znalosti_s2">
+              <span className="znalosti_s2_1">
+                <FaLanguage size="1.8em" />
               </span>
-              <p className="grid2_1 skuska_grid2_2">Ladislav Chvastaš</p>
+              <p className="znalosti_s2_2">Anglický jazyk</p>
+              <p className="znalosti_s2_3"> - B1</p>
             </div>
-            <div className="grid_15_85">
-              <span className="grid1_1">
-                <GrMail size="1.8em" />
+            <div className="znalosti_s3">
+              <span className="znalosti_s3_1">
+                <DiHtml5 size="1.8em" />
               </span>
-              <a className="grid1_2" href="mailto:ladislavchvastas@gmail.com">
-                ladislavchvastas@gmail.com
-              </a>
+              <p className="znalosti_s3_2">HTML</p>
+              <p className="znalosti_s3_3"> - pokročilý</p>
             </div>
-            <div className="grid_15_85">
-              <span className="grid1_1">
-                <BsFillTelephoneFill size="1.8em" />
+            <div className="znalosti_s4">
+              <span className="znalosti_s4_1">
+                <DiCss3 size="1.8em" />
               </span>
-              <a className="grid1_2" href="tel:+421 950 826 689">
-                +421 950 826 689
-              </a>
+              <p className="znalosti_s4_2">CSS</p>
+              <p className="znalosti_s4_3"> - pokročilý</p>
             </div>
-            <div className="grid_15_85">
-              <span className="grid1_1">
-                <FaMapMarkerAlt size="1.8em" />
+            <div className="znalosti_s5">
+              <span className="znalosti_s5_1">
+                <DiSass size="1.8em" />
               </span>
-              <p className="grid2_1">Vyšný Hrabovec 2, 09034</p>
+              <p className="znalosti_s5_2">SASS</p>
+              <p className="znalosti_s5_3"> - pokročilý</p>
             </div>
-            <div className="grid_15_85">
-              <span className="grid1_1">
-                <BsFillCalendarWeekFill size="1.8em" />
+            <div className="znalosti_s6">
+              <span className="znalosti_s6_1">
+                <DiJavascript1 size="1.8em" />
               </span>
-              <p className="grid2_1">14.8.1997</p>
+              <p className="znalosti_s6_2">JavaScript</p>
+              <p className="znalosti_s6_3"> - pokročilý</p>
             </div>
           </section>
 
-          <div className="border"></div>
+          <div className="border hidde2"></div>
 
-          <h3>Záujmy</h3>
-          <section className="zaujmy">
-            <div className="container_hobbies">
-              <div className="grid_25_25_25_25">
-                <span className="grid1_1 skuska_grid5_1">
+          <div className="udaje_zaujmy">
+            <section className="udaje">
+              <h3 className="udaje_s1">Osobné údaje</h3>
+              <div className="udaje_s2">
+                <span className="udaje_s2_1">
+                  <BsFillPersonFill size="1.8em" />
+                </span>
+                <p className="udaje_s2_2">Ladislav Chvastaš</p>
+              </div>
+              <div className="udaje_s3">
+                <span className="udaje_s3_1">
+                  <GrMail size="1.8em" />
+                </span>
+                <a
+                  className="udaje_s3_2"
+                  href="mailto:ladislavchvastas@gmail.com"
+                >
+                  ladislavchvastas@gmail.com
+                </a>
+              </div>
+              <div className="udaje_s4">
+                <span className="udaje_s4_1">
+                  <BsFillTelephoneFill size="1.8em" />
+                </span>
+                <a className="udaje_s4_2" href="tel:+421 950 826 689">
+                  +421 950 826 689
+                </a>
+              </div>
+              <div className="udaje_s5">
+                <span className="udaje_s5_1">
+                  <FaMapMarkerAlt size="1.8em" />
+                </span>
+                <p className="udaje_s5_2">Vyšný Hrabovec 2, 09034</p>
+              </div>
+              <div className="udaje_s6">
+                <span className="udaje_s6_1">
+                  <BsFillCalendarWeekFill size="1.8em" />
+                </span>
+                <p className="udaje_s6_2">14.8.1997</p>
+              </div>
+            </section>
+
+            <div className="border hidde1"></div>
+
+            <section className="zaujmy">
+              <h3 className="zaujmy_s1">Záujmy</h3>
+              <div className="zaujmy_s2">
+                <span>
                   <IoBarbellOutline size="1.8em" />
                 </span>
-                <p className="grid1_2">Fitness</p>
-                <span className="grid3_1">
+                <p>Fitness</p>
+              </div>
+
+              <div className="zaujmy_s3">
+                <span>
                   <BiRun size="1.8em" />
                 </span>
-                <p className="grid4_1">Beh</p>
+                <p>Beh</p>
               </div>
-              <div className="grid_25_25_25_25">
-                <span className="grid1_1">
+
+              <div className="zaujmy_s4">
+                <span>
                   <ImBooks size="1.8em" />
                 </span>
-                <p className="grid2_1">Knihy</p>
-                <span className="grid3_1">
+                <p>Knihy</p>
+              </div>
+              <div className="zaujmy_s5">
+                <span>
                   <BiJoystickAlt size="1.8em" />
                 </span>
-                <p className="grid4_2">Videohry</p>
+                <p>Videohry</p>
               </div>
-            </div>
-          </section>
+            </section>
+          </div>
         </div>
-        {/* <div className="border">
+        {/* <div className="border hidde">
           <h3>Vlastnosti</h3>
           <div className="flex">
           <span>
