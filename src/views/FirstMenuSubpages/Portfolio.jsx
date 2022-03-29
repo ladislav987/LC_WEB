f84ae51604c270f942568828ef55cfa678185cdc
Ladislav Chvastas <ladislavchvastas@gmail.com>
2022-03-29 10:55:52 +0200

    new portfolio grid

--- src/views/FirstMenuSubpages/Portfolio.jsx
+++ src/views/FirstMenuSubpages/Portfolio.jsx
@@ -3,6 +3,8 @@ import HomeButton from "../../component/HomeButton";
 import Svg from "../../component/Svg";
 import ThemeButton from "../../component/ThemeButton";
 
+import "../../scss/Portfolio.scss";
+
 const Portfolio = () => {
   return (
     <>
@@ -13,7 +15,30 @@ const Portfolio = () => {
           <ThemeButton />
         </nav>
       </header>
-      Portfolio !
+      <div className="gridMenu">
+        <div className="gridPortfolioContainer">
+          <a
+            className="itemPortfolio tipsCalculatorJS "
+            href="https://lc-tips-calculator-js.netlify.app/"
+          >
+            <p className="tipsCalculatorJSP">Tips Calculator JS</p>
+          </a>
+
+          <a
+            className="itemPortfolio tipsCalculatorReact "
+            href="http://lc-tips-calculator-react.netlify.app/"
+          >
+            <p className="tipsCalculatorReactP">Tips Calculator React</p>
+          </a>
+
+          <a
+            className="itemPortfolio calculatorJS "
+            href="https://lc-calculator.netlify.app/"
+          >
+            <p className="calculatorJSP">Calculator JS</p>
+          </a>
+        </div>
+      </div>
     </>
   );
 };
